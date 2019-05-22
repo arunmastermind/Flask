09cfee85161a6085d1b908dd6fe567cc61a25dbf
arunmastermind <arunmastermind.sci@gmail.com>
2019-05-23 03:27:45 +0530

    login and registration forms with a dummy user (hardcoded)

--- flaskBlog/flaskblog.py
+++ flaskBlog/flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9d993dc18ea4da0fbb8f43299c528a4e'
 
 posts = [
     {
@@ -26,6 +29,24 @@ def home():
 def about():
     return render_template('about.html', title='ABOUT')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created For {form.username.data}!!!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@example.com' and form.password.data == 'test@123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
